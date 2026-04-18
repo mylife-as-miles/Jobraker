253de6ccc6c5b0fba0101c46c93c8af6cc46cf1f
Osita Miles <siscomilesinfo@gmail.com>
2026-04-18 18:16:45 +0100

    Update fix-gemini.cjs

--- fix-gemini.cjs
+++ fix-gemini.cjs
@@ -6,7 +6,7 @@ const baseDirs = [
     "c:/Users/DELL PRECISION 5540/Documents/Jobraker/backend/supabase/supabase/functions"
 ];
 
-#hi
+#himm
 
 function processDir(dir) {
     if (!fs.existsSync(dir)) return;
