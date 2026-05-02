3467c7248e98d386ee22f337790f36bd590bd8fc
mylife-as-miles <mylife-as-miles@users.noreply.github.com>
2026-05-02 21:22:06 +0100

    Fix canonical sign-in route rendering

--- src/index.tsx
+++ src/index.tsx
@@ -130,7 +130,11 @@ function AnimatedRoutes() {
         />
 
         {/* Sign In Page */}
-        <Route path='/signin' element={<Navigate to={ROUTES.SIGNIN} replace />} />
+        <Route
+          path='/signin'
+          caseSensitive
+          element={<Navigate to={ROUTES.SIGNIN} replace />}
+        />
         <Route path='/login' element={<Navigate to={ROUTES.SIGNIN} replace />} />
         <Route
           path={ROUTES.SIGNIN}
